d0cca485fdbce83a6d74655f7af5af46c6c79eda
John Källén <johnkal@gmail.com>
2020-06-24 11:18:07 +0200

    Feature: more Hexagon instructions.

---
+++ src/Arch/Qualcomm/DecoratorOperand.cs
@@ -0,0 +1,87 @@
+#region License
+/* 
+ * Copyright (C) 1999-2020 John Källén.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#endregion
+
+using Reko.Core;
+using Reko.Core.Machine;
+using Reko.Core.Types;
+
+namespace Reko.Arch.Qualcomm
+{
+    public class DecoratorOperand : MachineOperand
+    {
+        public DecoratorOperand(PrimitiveType width, MachineOperand op) : base(width)
+        {
+            this.Operand = op;
+        }
+
+        public MachineOperand Operand { get; }
+        public int BitOffset { get; set;  }
+
+        public bool Inverted { get; set; }
+
+        public bool NewValue { get; set; }
+
+        public bool Complement { get; set; }
+
+        public bool Chop { get;  set; }
+
+        public bool Carry { get; set; }
+
+        public bool Sat { get; set; }
+        public bool Rnd { get; set; }
+
+        public override void Write(MachineInstructionWriter writer, MachineInstructionWriterOptions options)
+        {
+            if (Complement)
+                writer.WriteChar('~');
+            else if (Inverted)
+                writer.WriteChar('!');
+            this.Operand.Write(writer, options);
+            if (this.Width.BitSize < (int) Operand.Width.BitSize)
+            {
+                writer.WriteChar('.');
+                if (BitOffset == 0)
+                    writer.WriteChar('l');
+                else
+                    writer.WriteChar('h');
+            }
+            if (NewValue)
+            {
+                writer.WriteString(".new");
+            }
+            if (Chop)
+            {
+                writer.WriteString(":chop");
+            }
+            if (Carry)
+            {
+                writer.WriteString(":carry");
+            }
+            if (Rnd)
+            {
+                writer.WriteString(":rnd");
+            }
+            if (Sat)
+            {
+                writer.WriteString(":sat");
+            }
+        }
+    }
+}
--- src/Arch/Qualcomm/HexagonInstruction.cs
+++ src/Arch/Qualcomm/HexagonInstruction.cs
@@ -21,6 +21,7 @@
 using Reko.Core;
 using Reko.Core.Machine;
 using System;
+using System.Text;
 
 namespace Reko.Arch.Qualcomm
 {
@@ -36,31 +37,104 @@ namespace Reko.Arch.Qualcomm
         public Mnemonic Mnemonic { get; }
         public override int MnemonicAsInteger => (int) Mnemonic;
         public override string MnemonicAsString => Mnemonic.ToString();
-        public ParseType ParseType { get; internal set; }
+        public ParseType ParseType { get; set; }
+
+        public MachineOperand ConditionPredicate { get; set; }
+        public bool ConditionInverted { get; set; }
+        public bool ConditionPredicateNew { get; set; }
+
+        public DirectionHint DirectionHint { get; set; }
+
 
         public override void Render(MachineInstructionWriter writer, MachineInstructionWriterOptions options)
         {
-            if (Mnemonic == Mnemonic.ASSIGN)
+            if (ConditionPredicate != null)
             {
+                writer.WriteMnemonic("if");
+                writer.WriteString(" (");
+                if (ConditionInverted)
+                    writer.WriteChar('!');
+                ConditionPredicate.Write(writer, options);
+                if (ConditionPredicateNew)
+                    writer.WriteString(".new");
+                writer.WriteString(") ");
+            }
+            switch (Mnemonic)
+            {
+            case Mnemonic.ASSIGN:
                 RenderOperand(Operands[0], writer, options);
                 writer.WriteString(" = ");
                 RenderOperand(Operands[1], writer, options);
-            }
-            else
-            {
+                break;
+            case Mnemonic.ANDEQ:
+                RenderOperand(Operands[0], writer, options);
+                writer.WriteString(" &= ");
+                RenderOperand(Operands[1], writer, options);
+                break;
+            case Mnemonic.SUBEQ:
+                RenderOperand(Operands[0], writer, options);
+                writer.WriteString(" -= ");
+                RenderOperand(Operands[1], writer, options);
+                break;
+            case Mnemonic.OREQ:
+                RenderOperand(Operands[0], writer, options);
+                writer.WriteString(" |= ");
+                RenderOperand(Operands[1], writer, options);
+                break;
+            case Mnemonic.ADDEQ:
+                RenderOperand(Operands[0], writer, options);
+                writer.WriteString(" += ");
+                RenderOperand(Operands[1], writer, options);
+                break;
+            case Mnemonic.SIDEEFFECT:
+                RenderOperand(Operands[0], writer, options);
+                break;
+            default:
                 RenderMnemonic(writer, options);
                 RenderOperands(writer, options);
+                break;
             }
         }
 
         private void RenderMnemonic(MachineInstructionWriter writer, MachineInstructionWriterOptions options)
         {
-            writer.WriteMnemonic(Mnemonic.ToString());
+            var s = Mnemonic.ToString();
+            var sb = new StringBuilder();
+            for (int i = 0; i < s.Length; ++i)
+            {
+                char ch = s[i];
+                if (ch == '_' && i < s.Length - 1 && s[i + 1] == '_')
+                {
+                    sb.Append('.');
+                    ++i;
+                }
+                else
+                {
+                    sb.Append(ch);
+                }
+            }
+            if (DirectionHint != DirectionHint.None)
+            {
+                sb.Append(':');
+                if (DirectionHint == DirectionHint.Taken)
+                    sb.Append('t');
+                else
+                    sb.Append("nt");
+            }
+            writer.WriteMnemonic(sb.ToString());
         }
     }
 
     public enum ParseType
     {
         Duplex = 0b00,
+        End = 0b11,
+    }
+
+    public enum DirectionHint
+    {
+        None = 0,
+        NotTaken = 1,
+        Taken = 2,
     }
 }
